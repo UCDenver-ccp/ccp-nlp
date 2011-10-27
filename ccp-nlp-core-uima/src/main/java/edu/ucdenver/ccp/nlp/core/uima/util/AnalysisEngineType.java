d38265373187d19766acd31d25861fa17db4e7b6
Bill Baumgartner <bill.baumgartner@gmail.com>
2011-10-27 02:45:55 +0000

    Initial commit of a simple enum to distinguish between primitive and aggregate analysis engines

---
+++ ccp-nlp-core-uima/src/main/java/edu/ucdenver/ccp/nlp/core/uima/util/AnalysisEngineType.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.nlp.core.uima.util;
+
+/**
+ * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public enum AnalysisEngineType {
+	PRIMITIVE,
+	AGGREGATE
+}
