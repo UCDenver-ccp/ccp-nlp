57674fabb91a0c73abecb939e13dfcab5d710485
Bill Baumgartner <bill.baumgartner@gmail.com>
2011-10-27 03:35:37 +0000

    Use of ExternalResource in ABNER_AE is incorrect I think. ExternalResources use keys that must be unique. Attempting to load ABNER with two different models from AnalysisEngine Descriptions caused an error message stating that a duplicate key was detected and would be ignored, causing both ABNER instances to be loaded with identical models. In light of this, the ABNER_AE has been re-fitted to use a configuration parameter to pass in the model file.
    Also added a method to return the AnalysisEngineDescription (useful for creating Aggregate AEs using UimaFIT).

--- ccp-nlp-ext-uima-annotators-entitydetection/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/entitydetection/ABNER_AE.java
+++ ccp-nlp-ext-uima-annotators-entitydetection/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/entitydetection/ABNER_AE.java
@@ -27,13 +27,11 @@ import org.apache.log4j.Logger;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.resource.DataResource;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
-import org.apache.uima.util.InvalidXMLException;
-import org.uimafit.descriptor.ExternalResource;
+import org.uimafit.descriptor.ConfigurationParameter;
 import org.uimafit.factory.AnalysisEngineFactory;
-import org.uimafit.factory.ExternalResourceFactory;
+import org.uimafit.factory.ConfigurationParameterFactory;
 
 import edu.ucdenver.ccp.common.file.FileUtil;
 import edu.ucdenver.ccp.nlp.core.exception.InitializationException;
@@ -41,79 +39,105 @@ import edu.ucdenver.ccp.nlp.core.interfaces.ITagger;
 import edu.ucdenver.ccp.nlp.wrapper.abner.Abner_Util;
 
 /**
- * The class incorporates the ABNER entity identification system into the UIMA analysis engine framework. This class outputs 5 distinct annotation
- * types. In the CCPTypeSytem, all five types fall under the SemanticAnnotation classification.
+ * The class incorporates the ABNER entity identification system into the UIMA analysis engine
+ * framework. This class outputs 5 distinct annotation types. In the CCPTypeSytem, all five types
+ * fall under the SemanticAnnotation classification.
  * <p>
- * ABNER was developed by Burr Settles and is available here: http://pages.cs.wisc.edu/~bsettles/abner/
+ * ABNER was developed by Burr Settles and is available here:
+ * http://pages.cs.wisc.edu/~bsettles/abner/
  * 
  * @author William A. Baumgartner, Jr.
  * 
  */
 public class ABNER_AE extends EntityTagger_AE {
 	static Logger logger = Logger.getLogger(ABNER_AE.class);
-	public final static String MODEL_FILE_KEY = "AbnerModelFile";
-	
-	@ExternalResource(key=MODEL_FILE_KEY)
-	DataResource modelFileDR;
-	
-    public void initialize(UimaContext context) throws ResourceInitializationException { 
-		super.initialize(context);
-    	entityTagger = new Abner_Util();
+	// public final static String MODEL_FILE_KEY = "AbnerModelFile";
 
-		/* Get the model file as defined by the Resource section of the descriptor for this Analysis Engine */
-		String abnerModelFile = modelFileDR.getUri().getPath();
+	/**
+	 * ExternalResources such as MODEL_FILE_KEY can only be defined once and thus conflict if there
+	 * are more than one ABNER instances in a pipeline set up with different models - this may only
+	 * be a problem when initializing from AnalysisEngineDescriptors in an Aggregate.
+	 */
+	// @ExternalResource(key = MODEL_FILE_KEY)
+	// DataResource modelFileDR;
+
+	/**
+	 * Parameter name used in the UIMA descriptor file for the token attribute extractor
+	 * implementation to use
+	 */
+	public static final String PARAM_MODEL_FILE = ConfigurationParameterFactory.createConfigurationParameterName(
+			ABNER_AE.class, "modelFile");
+
+	/**
+	 * The name of the TokenAttributeExtractor implementation to use
+	 */
+	@ConfigurationParameter(mandatory = true, description = "path to the model file to load")
+	private File modelFile;
+
+	public void initialize(UimaContext context) throws ResourceInitializationException {
+		super.initialize(context);
+		entityTagger = new Abner_Util();
+
+		// /*
+		// * Get the model file as defined by the Resource section of the descriptor for this
+		// Analysis
+		// * Engine
+		// */
+		// String abnerModelFile = modelFileDR.getUri().getPath();
 
 		/* initialize the tagging system */
 		try {
-			String[] args = {abnerModelFile};
+			String[] args = { modelFile.getAbsolutePath() };
 			entityTagger.initialize(ITagger.ENTITY_TAGGER, args);
 		} catch (InitializationException e) {
 			throw new ResourceInitializationException(e);
 		}
-    }
-    
-    public static AnalysisEngine createAnalysisEngine(TypeSystemDescription tsd) 
-    		throws ResourceInitializationException {
-    			return createAnalysisEngine_BioCreative(tsd);
-    		}
-    		
-    		public static AnalysisEngine createAnalysisEngine_BioCreative(TypeSystemDescription tsd) 
-    		throws ResourceInitializationException {
-    			InputStream modelStream = ABNER_AE.class.getResourceAsStream("/abner/models/biocreative.crf");
-    			try {
-    				File modelFile = File.createTempFile("biocreative", "crf");
-    				FileUtil.copy(modelStream, modelFile);
-    				return createAnalysisEngine(tsd, modelFile);
-    			} catch (IOException e) {
-    				throw new ResourceInitializationException(e);
-    			}
-    		}
-    		
-    		public static AnalysisEngine createAnalysisEngine_NLPBA(TypeSystemDescription tsd) 
-    		throws ResourceInitializationException {
-    			InputStream modelStream = ABNER_AE.class.getResourceAsStream("/abner/models/nlpba.crf");
-    			try {
-    				File modelFile = File.createTempFile("nlpba", "crf");
-    				FileUtil.copy(modelStream, modelFile);
-    				return createAnalysisEngine(tsd, modelFile);
-    			} catch (IOException e) {
-    				throw new ResourceInitializationException(e);
-    			}
-    		}
-    		
-    		public static AnalysisEngine createAnalysisEngine(TypeSystemDescription tsd, File path) 
-    		throws ResourceInitializationException {
-    			AnalysisEngineDescription aed = AnalysisEngineFactory.createPrimitiveDescription(
-    					ABNER_AE.class, tsd); 
-    			try {
-    				ExternalResourceFactory.bindResource(aed, MODEL_FILE_KEY, path);
-    			}
-    			catch (InvalidXMLException x) {
-    				throw new ResourceInitializationException(x);
-    			}
-    			AnalysisEngine ae = AnalysisEngineFactory.createPrimitive(aed);
-    			return ae;
-    		}
+	}
 
+	public static AnalysisEngineDescription createAnalysisEngineDescription_BioCreative(TypeSystemDescription tsd)
+			throws ResourceInitializationException {
+		InputStream modelStream = ABNER_AE.class.getResourceAsStream("/abner/models/biocreative.crf");
+		try {
+			File modelFile = File.createTempFile("biocreative", "crf");
+			FileUtil.copy(modelStream, modelFile);
+			return createAnalysisEngineDescription(tsd, modelFile);
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		}
+	}
+
+	public static AnalysisEngineDescription createAnalysisEngineDescription_NLPBA(TypeSystemDescription tsd)
+			throws ResourceInitializationException {
+		InputStream modelStream = ABNER_AE.class.getResourceAsStream("/abner/models/nlpba.crf");
+		try {
+			File modelFile = File.createTempFile("nlpba", "crf");
+			FileUtil.copy(modelStream, modelFile);
+			return createAnalysisEngineDescription(tsd, modelFile);
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		}
+	}
+
+	public static AnalysisEngine createAnalysisEngine_BioCreative(TypeSystemDescription tsd)
+			throws ResourceInitializationException {
+		return AnalysisEngineFactory.createPrimitive(createAnalysisEngineDescription_BioCreative(tsd));
+	}
+
+	public static AnalysisEngine createAnalysisEngine_NLPBA(TypeSystemDescription tsd)
+			throws ResourceInitializationException {
+		return AnalysisEngineFactory.createPrimitive(createAnalysisEngineDescription_NLPBA(tsd));
+	}
+
+	public static AnalysisEngineDescription createAnalysisEngineDescription(TypeSystemDescription tsd, File modelFile)
+			throws ResourceInitializationException {
+		AnalysisEngineDescription aed = AnalysisEngineFactory.createPrimitiveDescription(ABNER_AE.class, tsd,
+				PARAM_MODEL_FILE, modelFile.getAbsolutePath());
+		// try {
+		// ExternalResourceFactory.bindResource(aed, MODEL_FILE_KEY, path);
+		// } catch (InvalidXMLException x) {
+		// throw new ResourceInitializationException(x);
+		// }
+		return aed;
+	}
 
 }
