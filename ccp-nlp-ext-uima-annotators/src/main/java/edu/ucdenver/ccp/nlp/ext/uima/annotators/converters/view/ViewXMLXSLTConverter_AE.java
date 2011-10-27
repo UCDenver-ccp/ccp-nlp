13c16bca499a6fa82eaf5042b55d18c9033d935b
Bill Baumgartner <bill.baumgartner@gmail.com>
2011-10-27 02:53:54 +0000

    Modified ViewXMLXSLTConverter to use an EntityResolver to get DTD info from the classpath. Also modified to produce plain text (was producing simplified XML).

--- ccp-nlp-ext-uima-annotators/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/converters/view/ViewXMLXSLTConverter_AE.java
+++ ccp-nlp-ext-uima-annotators/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/converters/view/ViewXMLXSLTConverter_AE.java
@@ -18,20 +18,30 @@
 
 package edu.ucdenver.ccp.nlp.ext.uima.annotators.converters.view;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.StringWriter;
 
-import javax.xml.transform.Result;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
@@ -40,7 +50,13 @@ import org.apache.uima.util.Logger;
 import org.uimafit.descriptor.ConfigurationParameter;
 import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.ConfigurationParameterFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.SAXException;
 
+import edu.ucdenver.ccp.common.file.CharacterEncoding;
+import edu.ucdenver.ccp.common.io.StreamUtil;
+import edu.ucdenver.ccp.common.reflection.ConstructorUtil;
 import edu.ucdenver.ccp.nlp.core.uima.util.UIMA_Util;
 
 /*
@@ -58,7 +74,14 @@ public class ViewXMLXSLTConverter_AE extends ViewConverter_AE {
 	@ConfigurationParameter(mandatory = true)
 	private String xsltFileName;
 
-	static Logger logger;
+	public static String PARAM_ENTITY_RESOLVER_CLASS_NAME = ConfigurationParameterFactory
+			.createConfigurationParameterName(ViewXMLXSLTConverter_AE.class, "entityResolverClassName");
+	@ConfigurationParameter(mandatory = true)
+	private String entityResolverClassName;
+
+	private static Logger logger;
+	private DocumentBuilder documentBuilder;
+	private Transformer transformer;
 
 	/*
 	 * (non-Javadoc)
@@ -68,7 +91,52 @@ public class ViewXMLXSLTConverter_AE extends ViewConverter_AE {
 	@Override
 	public void initialize(UimaContext context) throws ResourceInitializationException {
 		super.initialize(context);
+		initializeDocumentBuilder();
+		initalizeXsltTransformer();
+
 		logger = context.getLogger();
+	}
+
+	/**
+	 * @throws ResourceInitializationException
+	 */
+	private void initalizeXsltTransformer() throws ResourceInitializationException {
+		InputStream xslStream = null;
+		try {
+			xslStream = this.getClass().getResourceAsStream(xsltFileName);
+			if (xslStream == null) {
+				throw new ResourceInitializationException(new IllegalArgumentException(
+						"Null xslt file stream when reading:" + xsltFileName));
+			}
+			Source xsltSource = new StreamSource(xslStream);
+			TransformerFactory transFact = TransformerFactory.newInstance();
+			transformer = transFact.newTransformer(xsltSource);
+		} catch (TransformerConfigurationException e) {
+			throw new ResourceInitializationException(e);
+
+		} finally {
+			try {
+				if (xslStream != null)
+					xslStream.close();
+			} catch (IOException e) {
+				throw new ResourceInitializationException(e);
+			}
+		}
+
+	}
+
+	/**
+	 * @throws ResourceInitializationException
+	 */
+	private void initializeDocumentBuilder() throws ResourceInitializationException {
+		EntityResolver entityResolver = (EntityResolver) ConstructorUtil.invokeConstructor(entityResolverClassName);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		try {
+			documentBuilder = dbFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			throw new ResourceInitializationException(e);
+		}
+		documentBuilder.setEntityResolver(entityResolver);
 	}
 
 	@Override
@@ -85,46 +153,66 @@ public class ViewXMLXSLTConverter_AE extends ViewConverter_AE {
 
 	protected String convert(String input) throws AnalysisEngineProcessException {
 
-		InputStream xslStream = null;
-		String retval = "";
 		try {
-			TransformerFactory transFact = TransformerFactory.newInstance();
-			xslStream = this.getClass().getResourceAsStream(xsltFileName);
-			if (xslStream == null) {
-				throw new AnalysisEngineProcessException(new IllegalArgumentException(
-						"null xslt file stream when reading:" + xsltFileName));
+			Document document = documentBuilder.parse(StreamUtil.getEncodingSafeInputStream(new ByteArrayInputStream(
+					input.getBytes()), CharacterEncoding.UTF_8));
+			Source xmlSource = new DOMSource(document.getDocumentElement());
+//			OutputStream outStream = StreamUtil.getEncodingSafeOutputStream(new ByteArrayOutputStream(),
+//					CharacterEncoding.UTF_8);
+			
+			ByteArrayOutputStream outputStream = null;
+			ByteArrayInputStream bis = null;
+			try {
+			outputStream = new ByteArrayOutputStream();
+			transformer.transform(xmlSource, new StreamResult(outputStream));
+			bis = new ByteArrayInputStream(outputStream.toByteArray());
+			String documentText =  StreamUtil.toString(new InputStreamReader(bis, CharacterEncoding.UTF_8.getDecoder()));
+			documentText = removeXmlTags(documentText);
+			return documentText;
+			
+			} finally {
+				outputStream.close();
+				bis.close();
 			}
-			Source xsltSource = new javax.xml.transform.stream.StreamSource(xslStream);
-			Transformer trans = transFact.newTransformer(xsltSource);
-			StringReader sr = new StringReader(input);
-			Source xmlSource = new javax.xml.transform.stream.StreamSource(sr);
-			StringWriter sw = new StringWriter();
-			Result result = new javax.xml.transform.stream.StreamResult(sw);
-			trans.transform(xmlSource, result);
-			retval = sw.toString();
-			sw.close();
-			return retval;
 		} catch (TransformerConfigurationException e) {
 			throw new AnalysisEngineProcessException(e);
 		} catch (TransformerException e) {
 			throw new AnalysisEngineProcessException(e);
 		} catch (IOException e) {
 			throw new AnalysisEngineProcessException(e);
-		} finally {
-			try {
-				if (xslStream != null) {
-					xslStream.close();
-				}
-			} catch (IOException e) {
-				throw new AnalysisEngineProcessException(e);
-			}
-			// return retval;
+		} catch (SAXException e) {
+			throw new AnalysisEngineProcessException(e);
 		}
 	}
 
+	/**
+	 * @return
+	 */
+	private String removeXmlTags(String input) {
+		String transformed = input.replaceAll("</?PARAGRAPH>", "\n");
+		transformed = transformed.replaceAll("</?SECTION.*?>", "\n");
+		transformed = transformed.replaceAll("</?SUBSECTION.*?>", "\n");
+		transformed = transformed.replaceAll("</?FIGURE.*?>", "");
+		transformed = transformed.replaceAll("</?ITALICS>", "");
+		transformed = transformed.replaceAll("</?ABSTRACT.*?>", "");
+		transformed = transformed.replaceAll("</?TITLE>", "");
+		transformed = transformed.replaceAll("</?doc>", "");
+		transformed = transformed.replaceAll("<\\?xml version=\"1\\.0\" encoding=\"UTF-8\"\\?>", "");
+		return transformed;
+	}
+
 	public static AnalysisEngine createAnalysisEngine(TypeSystemDescription tsd, String sourceViewName,
-			String destinationViewName, String xsltFilename) throws ResourceInitializationException {
-		return AnalysisEngineFactory.createPrimitive(ViewXMLXSLTConverter_AE.class, tsd, PARAM_SOURCE_VIEW_NAME,
-				sourceViewName, PARAM_DESTINATION_VIEW_NAME, destinationViewName, PARAM_XSLT_FILE_NAME, xsltFilename);
+			String destinationViewName, String xsltFilename, Class<? extends EntityResolver> entityResolverClass)
+			throws ResourceInitializationException {
+		return AnalysisEngineFactory.createPrimitive(createAnalysisEngineDescription(tsd, sourceViewName,
+				destinationViewName, xsltFilename, entityResolverClass));
+		
+	}
+	public static AnalysisEngineDescription createAnalysisEngineDescription(TypeSystemDescription tsd,
+			String sourceViewName, String destinationViewName, String xsltFilename,
+			Class<? extends EntityResolver> entityResolverClass) throws ResourceInitializationException {
+		return AnalysisEngineFactory.createPrimitiveDescription(ViewXMLXSLTConverter_AE.class, tsd,
+				PARAM_SOURCE_VIEW_NAME, sourceViewName, PARAM_DESTINATION_VIEW_NAME, destinationViewName,
+				PARAM_XSLT_FILE_NAME, xsltFilename, PARAM_ENTITY_RESOLVER_CLASS_NAME, entityResolverClass.getName());
 	}
 }
