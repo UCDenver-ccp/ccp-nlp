7e6da918828df11fea3ddb8040a85a78001cc93e
Bill Baumgartner <bill.baumgartner@gmail.com>
2011-12-06 21:46:29 +0000

    Initial commit of a CollectionReader for reading Medline XML formatted files.
    OPEN - issue NLPIPES-1: Transfer relevant pipelines from BTRC_Demo project to the biojigsaw-networks-build project 
    http://compbio.ucdenver.edu:8080/browse/NLPIPES-1

---
+++ ccp-nlp-ext-uima-collections/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/collections/file/medline/MedlineXmlFileCollectionReader.java
@@ -0,0 +1,135 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.nlp.ext.uima.collections.file.medline;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.uimafit.descriptor.ConfigurationParameter;
+import org.uimafit.factory.ConfigurationParameterFactory;
+
+import edu.ucdenver.ccp.common.file.CharacterEncoding;
+import edu.ucdenver.ccp.common.io.StreamUtil;
+import edu.ucdenver.ccp.common.string.StringConstants;
+import edu.ucdenver.ccp.medline.parser.MedlineCitation;
+import edu.ucdenver.ccp.medline.parser.MedlineCitation.AbstractText;
+import edu.ucdenver.ccp.medline.parser.MedlineXmlDeserializer;
+import edu.ucdenver.ccp.nlp.core.document.GenericDocument;
+import edu.ucdenver.ccp.nlp.ext.uima.collections.file.BaseTextCollectionReader;
+
+/**
+ * This collection reader takes as input a file using the Medline XML format (as is returned from a
+ * PubMed query for instance) and returns documents containing the title and abstract for each
+ * Medline record in the input file. Title and abstract are separated by a line break.
+ * 
+ * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public class MedlineXmlFileCollectionReader extends BaseTextCollectionReader {
+
+	/* ==== Input file configuration ==== */
+	public static final String PARAM_MEDLINE_XML_FILE = ConfigurationParameterFactory.createConfigurationParameterName(
+			MedlineXmlFileCollectionReader.class, "medlineXmlFile");
+
+	@ConfigurationParameter(mandatory = true, description = "The file containing the Medline XML comprising this document collection")
+	protected File medlineXmlFile;
+
+	private MedlineXmlDeserializer medlineXmlDeserializer;
+
+	private GenericDocument nextDocument = null;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.ucdenver.ccp.nlp.ext.uima.collections.file.BaseTextCollectionReader#initializeImplementation
+	 * (org.apache.uima.UimaContext)
+	 */
+	@Override
+	protected void initializeImplementation(UimaContext context) throws ResourceInitializationException {
+		try {
+			medlineXmlDeserializer = new MedlineXmlDeserializer(StreamUtil.getEncodingSafeInputStream(medlineXmlFile,
+					CharacterEncoding.UTF_8));
+		} catch (IOException e) {
+			throw new ResourceInitializationException(e);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.ucdenver.ccp.nlp.ext.uima.collections.file.BaseTextCollectionReader#
+	 * countDocumentsInCollection()
+	 */
+	@Override
+	protected int countDocumentsInCollection() throws IOException {
+		MedlineXmlDeserializer deserializer = new MedlineXmlDeserializer(StreamUtil.getEncodingSafeInputStream(
+				medlineXmlFile, CharacterEncoding.UTF_8));
+		int documentCount = 0;
+		while (deserializer.hasNext()) {
+			documentCount++;
+			deserializer.next();
+		}
+		return documentCount;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.ucdenver.ccp.nlp.ext.uima.collections.file.BaseTextCollectionReader#skip()
+	 */
+	@Override
+	protected void skip() throws ResourceInitializationException {
+		int numSkipped = 0;
+		while (numSkipped < numberToSkip && medlineXmlDeserializer.hasNext()) {
+			numSkipped++;
+			medlineXmlDeserializer.next();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.ucdenver.ccp.nlp.ext.uima.collections.file.BaseTextCollectionReader#hasNextDocument()
+	 */
+	@Override
+	protected boolean hasNextDocument() throws IOException, CollectionException {
+		if (nextDocument == null) {
+			if (medlineXmlDeserializer.hasNext()) {
+				MedlineCitation nextCitation = medlineXmlDeserializer.next();
+				StringBuffer documentText = new StringBuffer();
+				documentText.append(nextCitation.getArticle().getArticleTitle());
+				for (AbstractText abstractText : nextCitation.getArticle().getTheAbstract().getAbstractTexts())
+					documentText.append(StringConstants.NEW_LINE + abstractText.getAbstractText());
+				nextDocument = new GenericDocument(nextCitation.getPmid().getPmid());
+				nextDocument.setDocumentText(documentText.toString());
+				return true;
+			}
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.ucdenver.ccp.nlp.ext.uima.collections.file.BaseTextCollectionReader#getNextDocument()
+	 */
+	@Override
+	protected GenericDocument getNextDocument() throws CollectionException, IOException {
+		if (!hasNext())
+			throw new NoSuchElementException();
+		GenericDocument gd = nextDocument;
+		nextDocument = null;
+		return gd;
+
+	}
+
+}
