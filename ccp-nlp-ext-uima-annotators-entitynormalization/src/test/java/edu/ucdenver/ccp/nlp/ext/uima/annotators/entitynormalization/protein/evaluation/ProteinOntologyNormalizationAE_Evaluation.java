e28f9736c635a6c9ae0fc29ad78fd4590282e71f
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-01-17 20:56:09 +0000

    IN PROGRESS - issue BEL-1: PRO normalization over BEL small corpus http://compbio.ucdenver.edu:8080/browse/BEL-1
    Made the sentence detector descriptor a parameter for the getAggregateDescription() method

--- ccp-nlp-ext-uima-annotators-entitynormalization/src/test/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/entitynormalization/protein/evaluation/ProteinOntologyNormalizationAE_Evaluation.java
+++ ccp-nlp-ext-uima-annotators-entitynormalization/src/test/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/entitynormalization/protein/evaluation/ProteinOntologyNormalizationAE_Evaluation.java
@@ -5,11 +5,13 @@ package edu.ucdenver.ccp.nlp.ext.uima.annotators.entitynormalization.protein.eva
 
 import java.io.File;
 
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.uimafit.factory.AnalysisEngineFactory;
 import org.uimafit.factory.TypeSystemDescriptionFactory;
 
 import edu.ucdenver.ccp.common.test.DefaultTestCase;
@@ -17,6 +19,8 @@ import edu.ucdenver.ccp.nlp.core.uima.util.AnalysisEngineType;
 import edu.ucdenver.ccp.nlp.core.uima.util.TypeSystemUtil;
 import edu.ucdenver.ccp.nlp.ext.uima.annotators.entitynormalization.gene.evaluation.BioCreative3GnEvaluationPipeline;
 import edu.ucdenver.ccp.nlp.ext.uima.annotators.entitynormalization.protein.ProOntologyProteinNormalization_AAE;
+import edu.ucdenver.ccp.nlp.ext.uima.annotators.sentencedetectors.LingPipeSentenceDetector_AE;
+import edu.ucdenver.ccp.nlp.ext.uima.annotators.sentencedetectors.SentenceDetector_AE;
 
 /**
  * Evaluates the HomologeneGroupGeneNormalization pipeline against gold standard corpora
@@ -36,10 +40,12 @@ public class ProteinOntologyNormalizationAE_Evaluation extends DefaultTestCase {
 	@Test
 	public void evaluateOnBioCreative3TrainingDataSet() throws AnalysisEngineProcessException,
 			ResourceInitializationException {
+		AnalysisEngineDescription sentenceDetectorDescription = AnalysisEngineFactory.createPrimitiveDescription(
+				LingPipeSentenceDetector_AE.class, TSD,
+				SentenceDetector_AE.PARAM_TREAT_LINE_BREAKS_AS_SENTENCE_BOUNDARIES, true);
 		BioCreative3GnEvaluationPipeline evalPipeline = new BioCreative3GnEvaluationPipeline(TSD);
-		evalPipeline.evaluateNormalizationPipeline(
-				ProOntologyProteinNormalization_AAE.getAggregateDescription(TSD, PRO_DICTIONARY_DIRECTORY),
-				AnalysisEngineType.AGGREGATE);
+		evalPipeline.evaluateNormalizationPipeline(ProOntologyProteinNormalization_AAE.getAggregateDescription(TSD,
+				PRO_DICTIONARY_DIRECTORY, sentenceDetectorDescription), AnalysisEngineType.AGGREGATE);
 	}
 
 }
