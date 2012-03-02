9294e966ae43a2d6d82b50292b0911d788026720
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-03-02 05:43:51 +0000

    Added the addAnnotationAttribute(Annotation annotation, String attributeType, Object attribute) method

--- ccp-nlp-ext-uima-shims/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/shims/annotation/impl/CcpAnnotationDecorator.java
+++ ccp-nlp-ext-uima-shims/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/shims/annotation/impl/CcpAnnotationDecorator.java
@@ -3,20 +3,46 @@
  */
 package edu.ucdenver.ccp.nlp.ext.uima.shims.annotation.impl;
 
+import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 
+import edu.ucdenver.ccp.nlp.core.annotation.Span;
 import edu.ucdenver.ccp.nlp.core.annotation.TextAnnotationUtil;
 import edu.ucdenver.ccp.nlp.core.uima.annotation.CCPTextAnnotation;
 import edu.ucdenver.ccp.nlp.core.uima.annotation.impl.WrappedCCPTextAnnotation;
+import edu.ucdenver.ccp.nlp.core.uima.util.UIMA_Annotation_Util;
 import edu.ucdenver.ccp.nlp.ext.uima.shims.annotation.AnnotationDecorator;
 
 /**
  * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
- *
+ * 
  */
 public abstract class CcpAnnotationDecorator<T> implements AnnotationDecorator<T> {
 
-	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.ucdenver.ccp.nlp.ext.uima.shims.annotation.AnnotationDecorator#createAnnotation(org.apache
+	 * .uima.jcas.JCas, java.lang.String, edu.ucdenver.ccp.nlp.core.annotation.Span)
+	 */
+	@Override
+	public Annotation newAnnotation(JCas jCas, String type, Span span) {
+		return UIMA_Annotation_Util.createCCPTextAnnotation(type, span.getSpanStart(), span.getSpanEnd(), jCas);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.ucdenver.ccp.nlp.ext.uima.shims.annotation.AnnotationDecorator#addAnnotationAttribute
+	 * (org.apache.uima.jcas.tcas.Annotation, java.lang.String, java.lang.Object)
+	 */
+	@Override
+	public void addAnnotationAttribute(Annotation annotation, String attributeType, Object attributeValue) {
+		addPrimitiveSlot(annotation, attributeType, attributeValue);
+	}
+
 	/**
 	 * @param annotation
 	 */
@@ -28,7 +54,6 @@ public abstract class CcpAnnotationDecorator<T> implements AnnotationDecorator<T
 							this.getClass().getName(), annotation.getClass().getName()));
 	}
 
-	
 	protected void addPrimitiveSlot(Annotation annotation, String slotName, Object primitiveSlotValue) {
 		checkType(annotation);
 		WrappedCCPTextAnnotation wrappedTa = new WrappedCCPTextAnnotation((CCPTextAnnotation) annotation);
@@ -38,8 +63,5 @@ public abstract class CcpAnnotationDecorator<T> implements AnnotationDecorator<T
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
-	
 
 }
