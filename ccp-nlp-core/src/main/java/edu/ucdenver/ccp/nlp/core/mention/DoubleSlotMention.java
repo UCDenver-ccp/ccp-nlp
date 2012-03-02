0f8418bdcc4add9528ea5a90fecae8e5059b01a9
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-03-02 16:00:02 +0000

    Initial commit of the DoubleSlotMention class

---
+++ ccp-nlp-core/src/main/java/edu/ucdenver/ccp/nlp/core/mention/DoubleSlotMention.java
@@ -0,0 +1,37 @@
+package edu.ucdenver.ccp.nlp.core.mention;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+
+public abstract class DoubleSlotMention extends PrimitiveSlotMention<Double> {
+	private static Logger logger = Logger.getLogger(DoubleSlotMention.class);
+
+//	public FloatSlotMention(String mentionName, IMentionTraversalTracker traversalTracker, Object... wrappedObjectPlusGlobalVars) {
+//		super(mentionName,traversalTracker, wrappedObjectPlusGlobalVars);
+//	}
+	
+	public DoubleSlotMention(IMentionTraversalTracker traversalTracker, Object... wrappedObjectPlusGlobalVars) {
+		super(traversalTracker, wrappedObjectPlusGlobalVars);
+	}
+
+	@Override
+	public String getStringRepresentation(int indentLevel, boolean showReferencingAnnotationInfo) {
+		List<Double> slotValuesList = Collections.list(Collections.enumeration(this.getSlotValues()));
+		Collections.sort(slotValuesList);
+		return getStringRepresentation(indentLevel, slotValuesList);
+	}
+
+	@Override
+	public void addSlotValueAsString(String slotValue) {
+		try {
+			Double floatSlotValue = Double.parseDouble(slotValue.trim());
+			addSlotValue(floatSlotValue);
+		} catch (Exception e) {
+			logger.error("Attempting to add slot value that is not an Float (" + slotValue
+					+ ") to an FloatSlotValue. This value has not been added to the mention hierarchy.");
+		}
+	}
+}
