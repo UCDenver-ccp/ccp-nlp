f3d5da9f0189f133cb6946413143d96f366bd643
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-04-03 18:04:49 +0000

    Added buildModelOrganismConceptMapperDictionary() method

--- ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/dictionary/eg/EntrezGeneDictionaryFactory.java
+++ ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/dictionary/eg/EntrezGeneDictionaryFactory.java
@@ -6,6 +6,7 @@ package edu.ucdenver.ccp.nlp.wrapper.conceptmapper.dictionary.eg;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -13,6 +14,7 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 import edu.ucdenver.ccp.common.collections.CollectionsUtil;
+import edu.ucdenver.ccp.datasource.identifiers.ncbi.taxonomy.NcbiTaxonomyID;
 import edu.ucdenver.ccp.fileparsers.field.GeneNameOrSymbol;
 import edu.ucdenver.ccp.fileparsers.ncbi.gene.EntrezGeneInfoFileData;
 import edu.ucdenver.ccp.fileparsers.ncbi.gene.EntrezGeneInfoFileParser;
@@ -30,7 +32,47 @@ public class EntrezGeneDictionaryFactory {
 
 	private static final int MINIMUM_TERM_LENGTH = 1;
 
-	public static File buildConceptMapperDictionary(File workDirectory, boolean cleanWorkDirectory) throws IOException {
+	/**
+	 * List of organisms taken from the NCBI Taxonomy home page:
+	 * http://www.ncbi.nlm.nih.gov/Taxonomy/
+	 * 
+	 * @param workDirectory
+	 * @param cleanWorkDirectory
+	 * @return
+	 * @throws IOException
+	 */
+	public static File buildModelOrganismConceptMapperDictionary(File workDirectory, boolean cleanWorkDirectory)
+			throws IOException {
+		/* @formatter:off */
+		Set<NcbiTaxonomyID> taxonomyIdsToInclude = CollectionsUtil.createSet(
+				new NcbiTaxonomyID(3702), // Arabidopsis thaliana
+				new NcbiTaxonomyID(9913), // Bos taurus
+				new NcbiTaxonomyID(6239), // Caenorhabditis elegans
+				new NcbiTaxonomyID(3055), // Chlamydomonas reinhardtii
+				new NcbiTaxonomyID(7955), // Danio rerio
+				new NcbiTaxonomyID(44689), // Dictyostelium discoideum
+				new NcbiTaxonomyID(7227), // Drosophila melanogaster
+				new NcbiTaxonomyID(562), // Escherichia coli
+				new NcbiTaxonomyID(11103), // Hepatitis C virus
+				new NcbiTaxonomyID(9606), // Homo sapiens
+				new NcbiTaxonomyID(10090), // Mus musculus
+				new NcbiTaxonomyID(2104), // Mycoplasma pneumoniae
+				new NcbiTaxonomyID(4530), // Oryza sativa
+				new NcbiTaxonomyID(5833), // Plasmodium falciparum
+				new NcbiTaxonomyID(4754), // Pneumocystis carinii
+				new NcbiTaxonomyID(10116), // Rattus norvegicus
+				new NcbiTaxonomyID(4932), // Saccharomyces cerevisiae
+				new NcbiTaxonomyID(4896), // Schizosaccharomyces pombe
+				new NcbiTaxonomyID(31033), // Takifugu rubripes
+				new NcbiTaxonomyID(8355), // Xenopus laevis
+				new NcbiTaxonomyID(4577) // Zea mays
+				);
+		/* @formatter:on */
+		return buildConceptMapperDictionary(workDirectory, cleanWorkDirectory, taxonomyIdsToInclude);
+	}
+
+	public static File buildConceptMapperDictionary(File workDirectory, boolean cleanWorkDirectory,
+			Set<NcbiTaxonomyID> taxonomyIdsToInclude) throws IOException {
 
 		File dictionaryFile = new File(workDirectory, "cmDict-EntrezGene.xml");
 		ConceptMapperDictionaryBuilder dictBuilder = new ConceptMapperDictionaryBuilder(dictionaryFile,
@@ -42,30 +84,37 @@ public class EntrezGeneDictionaryFactory {
 				logger.info("Progress: " + (count - 1));
 
 			EntrezGeneInfoFileData dataRecord = parser.next();
+			NcbiTaxonomyID taxonID = dataRecord.getTaxonID();
+			boolean useRecord = true;
+			if (taxonomyIdsToInclude != null && !taxonomyIdsToInclude.isEmpty()) {
+				useRecord = taxonomyIdsToInclude.contains(taxonID);
+			}
 
-			List<String> synonymStrs = new ArrayList<String>();
+			if (useRecord) {
+				List<String> synonymStrs = new ArrayList<String>();
 
-			GeneNameOrSymbol nomSymbol = dataRecord.getSymbolFromNomenclatureAuthority();
-			if (nomSymbol != null)
-				synonymStrs.add(nomSymbol.getDataElement());
-			GeneNameOrSymbol nomName = dataRecord.getFullNameFromNomenclatureAuthority();
-			if (nomName != null)
-				synonymStrs.add(nomName.getDataElement());
-			GeneNameOrSymbol symbol = dataRecord.getSymbol();
-			if (symbol != null)
-				synonymStrs.add(symbol.getDataElement());
-			Set<GeneNameOrSymbol> synonyms = dataRecord.getSynonyms();
-			if (synonyms != null)
-				for (GeneNameOrSymbol syn : synonyms)
-					synonymStrs.add(syn.getDataElement());
-			Set<GeneNameOrSymbol> otherDesignations = dataRecord.getOtherDesignations();
-			if (otherDesignations != null)
-				for (GeneNameOrSymbol desig : otherDesignations)
-					synonymStrs.add(desig.getDataElement());
+				GeneNameOrSymbol nomSymbol = dataRecord.getSymbolFromNomenclatureAuthority();
+				if (nomSymbol != null)
+					synonymStrs.add(nomSymbol.getDataElement());
+				GeneNameOrSymbol nomName = dataRecord.getFullNameFromNomenclatureAuthority();
+				if (nomName != null)
+					synonymStrs.add(nomName.getDataElement());
+				GeneNameOrSymbol symbol = dataRecord.getSymbol();
+				if (symbol != null)
+					synonymStrs.add(symbol.getDataElement());
+				Set<GeneNameOrSymbol> synonyms = dataRecord.getSynonyms();
+				if (synonyms != null)
+					for (GeneNameOrSymbol syn : synonyms)
+						synonymStrs.add(syn.getDataElement());
+				Set<GeneNameOrSymbol> otherDesignations = dataRecord.getOtherDesignations();
+				if (otherDesignations != null)
+					for (GeneNameOrSymbol desig : otherDesignations)
+						synonymStrs.add(desig.getDataElement());
 
-			synonymStrs = filterSynonyms(synonymStrs);
+				synonymStrs = filterSynonyms(synonymStrs);
 
-			dictBuilder.addEntry(dataRecord.getGeneID().getDataElement().toString(), synonymStrs);
+				dictBuilder.addEntry(dataRecord.getGeneID().getDataElement().toString(), synonymStrs);
+			}
 		}
 
 		dictBuilder.close();
@@ -95,15 +144,21 @@ public class EntrezGeneDictionaryFactory {
 	/**
 	 * @param args
 	 *            args[0] = work directory<br>
-	 *            args[1] = clean work directory (true/false)
+	 *            args[1] = clean work directory (true/false) <br>
+	 *            args[2+] = taxonomy Ids to include
 	 */
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
 		File workDirectory = new File(args[0]);
 		boolean cleanWorkDirectory = Boolean.valueOf(args[1]);
+		Set<NcbiTaxonomyID> taxonomyIdsToInclude = new HashSet<NcbiTaxonomyID>();
+		for (int i = 2; i < args.length; i++) {
+			taxonomyIdsToInclude.add(new NcbiTaxonomyID(args[i]));
+		}
 
 		try {
-			EntrezGeneDictionaryFactory.buildConceptMapperDictionary(workDirectory, cleanWorkDirectory);
+			EntrezGeneDictionaryFactory.buildConceptMapperDictionary(workDirectory, cleanWorkDirectory,
+					taxonomyIdsToInclude);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
