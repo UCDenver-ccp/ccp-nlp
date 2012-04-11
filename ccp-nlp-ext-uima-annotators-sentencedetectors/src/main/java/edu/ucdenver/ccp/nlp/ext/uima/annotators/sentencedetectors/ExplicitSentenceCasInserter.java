e0668c43997a685e2fcae3c06ae7a5c216aa2768
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-04-11 03:56:10 +0000

    Added a constant to define the sentence annotation type that is created by the ExplicitSentenceCasInserter

--- ccp-nlp-ext-uima-annotators-sentencedetectors/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/sentencedetectors/ExplicitSentenceCasInserter.java
+++ ccp-nlp-ext-uima-annotators-sentencedetectors/src/main/java/edu/ucdenver/ccp/nlp/ext/uima/annotators/sentencedetectors/ExplicitSentenceCasInserter.java
@@ -10,9 +10,14 @@ import edu.ucdenver.ccp.nlp.ext.uima.types.Sentence;
 
 /**
  * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
- *
+ * 
  */
 public class ExplicitSentenceCasInserter implements SentenceCasInserter {
+
+	/**
+	 * The annotation class inserted by this {@link SentenceCasInserter} implementation
+	 */
+	public static final Class<? extends Annotation> SENTENCE_ANNOTATION_CLASS = Sentence.class;
 
 	/**
 	 * Inserts explicitly defined {@link Sentence} annotations into the CAS
