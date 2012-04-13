462594da353463c9eb45fb1cd09184d0a523fff6
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-04-13 19:38:24 +0000

    Added the CleanDirectory dependency

--- ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/dictionary/eg/EntrezGeneDictionaryFactory.java
+++ ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/dictionary/eg/EntrezGeneDictionaryFactory.java
@@ -6,14 +6,13 @@ package edu.ucdenver.ccp.nlp.wrapper.conceptmapper.dictionary.eg;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 import edu.ucdenver.ccp.common.collections.CollectionsUtil;
+import edu.ucdenver.ccp.common.file.FileUtil.CleanDirectory;
 import edu.ucdenver.ccp.datasource.identifiers.ncbi.taxonomy.NcbiTaxonomyID;
 import edu.ucdenver.ccp.fileparsers.field.GeneNameOrSymbol;
 import edu.ucdenver.ccp.fileparsers.ncbi.gene.EntrezGeneInfoFileData;
@@ -41,7 +40,7 @@ public class EntrezGeneDictionaryFactory {
 	 * @return
 	 * @throws IOException
 	 */
-	public static File buildModelOrganismConceptMapperDictionary(File workDirectory, boolean cleanWorkDirectory)
+	public static File buildModelOrganismConceptMapperDictionary(File workDirectory, CleanDirectory cleanWorkDirectory)
 			throws IOException {
 		/* @formatter:off */
 		Set<NcbiTaxonomyID> taxonomyIdsToInclude = CollectionsUtil.createSet(
@@ -71,15 +70,15 @@ public class EntrezGeneDictionaryFactory {
 		return buildConceptMapperDictionary(workDirectory, cleanWorkDirectory, taxonomyIdsToInclude);
 	}
 
-	public static File buildConceptMapperDictionary(File workDirectory, boolean cleanWorkDirectory,
+	public static File buildConceptMapperDictionary(File workDirectory, CleanDirectory cleanWorkDirectory,
 			Set<NcbiTaxonomyID> taxonomyIdsToInclude) throws IOException {
 
 		File dictionaryFile = new File(workDirectory, "cmDict-EntrezGene.xml");
 		ConceptMapperDictionaryBuilder dictBuilder = new ConceptMapperDictionaryBuilder(dictionaryFile,
 				MINIMUM_TERM_LENGTH);
 		int count = 0;
-		for (EntrezGeneInfoFileParser parser = new EntrezGeneInfoFileParser(workDirectory, cleanWorkDirectory); parser
-				.hasNext();) {
+		boolean doClean = cleanWorkDirectory.equals(CleanDirectory.YES);
+		for (EntrezGeneInfoFileParser parser = new EntrezGeneInfoFileParser(workDirectory, doClean); parser.hasNext();) {
 			if (count++ % 100000 == 0)
 				logger.info("Progress: " + (count - 1));
 
@@ -141,26 +140,26 @@ public class EntrezGeneDictionaryFactory {
 		return filteredSyns;
 	}
 
-	/**
-	 * @param args
-	 *            args[0] = work directory<br>
-	 *            args[1] = clean work directory (true/false) <br>
-	 *            args[2+] = taxonomy Ids to include
-	 */
-	public static void main(String[] args) {
-		BasicConfigurator.configure();
-		File workDirectory = new File(args[0]);
-		boolean cleanWorkDirectory = Boolean.valueOf(args[1]);
-		Set<NcbiTaxonomyID> taxonomyIdsToInclude = new HashSet<NcbiTaxonomyID>();
-		for (int i = 2; i < args.length; i++) {
-			taxonomyIdsToInclude.add(new NcbiTaxonomyID(args[i]));
-		}
-
-		try {
-			EntrezGeneDictionaryFactory.buildConceptMapperDictionary(workDirectory, cleanWorkDirectory,
-					taxonomyIdsToInclude);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+	// /**
+	// * @param args
+	// * args[0] = work directory<br>
+	// * args[1] = clean work directory (true/false) <br>
+	// * args[2+] = taxonomy Ids to include
+	// */
+	// public static void main(String[] args) {
+	// BasicConfigurator.configure();
+	// File workDirectory = new File(args[0]);
+	// boolean cleanWorkDirectory = Boolean.valueOf(args[1]);
+	// Set<NcbiTaxonomyID> taxonomyIdsToInclude = new HashSet<NcbiTaxonomyID>();
+	// for (int i = 2; i < args.length; i++) {
+	// taxonomyIdsToInclude.add(new NcbiTaxonomyID(args[i]));
+	// }
+	//
+	// try {
+	// EntrezGeneDictionaryFactory.buildConceptMapperDictionary(workDirectory, cleanWorkDirectory,
+	// taxonomyIdsToInclude);
+	// } catch (IOException e) {
+	// throw new RuntimeException(e);
+	// }
+	// }
 }
