c9e3d561e2cb571c1187be5ad0312f3aea417308
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-04-13 19:37:56 +0000

    Added a factory method from the class that was removed from the uima-annotators-entitydetection project

--- ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/ConceptMapperAggregateFactory.java
+++ ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/ConceptMapperAggregateFactory.java
@@ -3,14 +3,17 @@
  */
 package edu.ucdenver.ccp.nlp.wrapper.conceptmapper;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.conceptMapper.support.stemmer.Stemmer;
+import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.uimafit.factory.AnalysisEngineFactory;
 
+import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.ConceptMapperFactory.SearchStrategyParamValue;
 import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.ConceptMapperFactory.TokenNormalizerConfigParam.CaseMatchParamValue;
 import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.tokenizer.OffsetTokenizerFactory;
 
@@ -20,6 +23,39 @@ import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.tokenizer.OffsetTokenizerFacto
  */
 public class ConceptMapperAggregateFactory {
 
+	
+	/**
+	 * Returns an aggregate description for a UIMA pipeline containing the OffsetTokenizer followed
+	 * by the ConceptMapper
+	 * 
+	 * @param tsd
+	 * @param dictionaryFile
+	 * @param caseMatchParamValue
+	 * @param searchStrategyParamValue
+	 * @param spanFeatureStructureClass 
+	 * @return
+	 * @throws UIMAException
+	 * @throws IOException
+	 */
+	public static AnalysisEngineDescription getOffsetTokenizerConceptMapperPipelineDescription(TypeSystemDescription tsd,
+			File dictionaryFile, CaseMatchParamValue caseMatchParamValue,
+			SearchStrategyParamValue searchStrategyParamValue, Class<? extends Annotation> spanFeatureStructureClass) throws UIMAException, IOException {
+
+		/* Init the tokenizer */
+		Object[] tokenizerConfigData = OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue);
+		AnalysisEngineDescription offsetTokenizerDescription = OffsetTokenizerFactory.buildOffsetTokenizerDescription(
+				tsd, tokenizerConfigData);
+
+		/* Init the concept mapper */
+		AnalysisEngineDescription conceptMapperDescription = ConceptMapperFactory.buildConceptMapperDescription(tsd,
+				dictionaryFile, caseMatchParamValue, searchStrategyParamValue, spanFeatureStructureClass,
+				offsetTokenizerDescription);
+
+		return AnalysisEngineFactory.createAggregateDescription(offsetTokenizerDescription, conceptMapperDescription);
+
+	}
+	
+	
 	public static AnalysisEngineDescription buildConceptMapperOffsetTokenizerAggregate(
 			AnalysisEngineDescription conceptMapperDesc, TypeSystemDescription tsd,
 			CaseMatchParamValue caseMatchParamValue, Class<Stemmer> stemmerClass, String tokenDelimiters)
