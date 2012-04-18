dd652ae1468604613e580ab7e78030613268b63b
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-04-18 17:16:27 +0000

    Modified to take as input the gene_info file directly (as an option - it can still downloaded if needed)

--- ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/dictionary/eg/EntrezGeneDictionaryFactory.java
+++ ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/dictionary/eg/EntrezGeneDictionaryFactory.java
@@ -12,6 +12,7 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 import edu.ucdenver.ccp.common.collections.CollectionsUtil;
+import edu.ucdenver.ccp.common.file.CharacterEncoding;
 import edu.ucdenver.ccp.common.file.FileUtil.CleanDirectory;
 import edu.ucdenver.ccp.datasource.identifiers.ncbi.taxonomy.NcbiTaxonomyID;
 import edu.ucdenver.ccp.fileparsers.field.GeneNameOrSymbol;
@@ -31,6 +32,32 @@ public class EntrezGeneDictionaryFactory {
 
 	private static final int MINIMUM_TERM_LENGTH = 1;
 
+	/* @formatter:off */
+	private static final Set<NcbiTaxonomyID> MODEL_ORG_TAX_IDS = CollectionsUtil.createSet(
+			new NcbiTaxonomyID(3702), // Arabidopsis thaliana
+			new NcbiTaxonomyID(9913), // Bos taurus
+			new NcbiTaxonomyID(6239), // Caenorhabditis elegans
+			new NcbiTaxonomyID(3055), // Chlamydomonas reinhardtii
+			new NcbiTaxonomyID(7955), // Danio rerio
+			new NcbiTaxonomyID(44689), // Dictyostelium discoideum
+			new NcbiTaxonomyID(7227), // Drosophila melanogaster
+			new NcbiTaxonomyID(562), // Escherichia coli
+			new NcbiTaxonomyID(11103), // Hepatitis C virus
+			new NcbiTaxonomyID(9606), // Homo sapiens
+			new NcbiTaxonomyID(10090), // Mus musculus
+			new NcbiTaxonomyID(2104), // Mycoplasma pneumoniae
+			new NcbiTaxonomyID(4530), // Oryza sativa
+			new NcbiTaxonomyID(5833), // Plasmodium falciparum
+			new NcbiTaxonomyID(4754), // Pneumocystis carinii
+			new NcbiTaxonomyID(10116), // Rattus norvegicus
+			new NcbiTaxonomyID(4932), // Saccharomyces cerevisiae
+			new NcbiTaxonomyID(4896), // Schizosaccharomyces pombe
+			new NcbiTaxonomyID(31033), // Takifugu rubripes
+			new NcbiTaxonomyID(8355), // Xenopus laevis
+			new NcbiTaxonomyID(4577) // Zea mays
+			);
+	/* @formatter:on */
+
 	/**
 	 * List of organisms taken from the NCBI Taxonomy home page:
 	 * http://www.ncbi.nlm.nih.gov/Taxonomy/
@@ -42,43 +69,43 @@ public class EntrezGeneDictionaryFactory {
 	 */
 	public static File buildModelOrganismConceptMapperDictionary(File workDirectory, CleanDirectory cleanWorkDirectory)
 			throws IOException {
-		/* @formatter:off */
-		Set<NcbiTaxonomyID> taxonomyIdsToInclude = CollectionsUtil.createSet(
-				new NcbiTaxonomyID(3702), // Arabidopsis thaliana
-				new NcbiTaxonomyID(9913), // Bos taurus
-				new NcbiTaxonomyID(6239), // Caenorhabditis elegans
-				new NcbiTaxonomyID(3055), // Chlamydomonas reinhardtii
-				new NcbiTaxonomyID(7955), // Danio rerio
-				new NcbiTaxonomyID(44689), // Dictyostelium discoideum
-				new NcbiTaxonomyID(7227), // Drosophila melanogaster
-				new NcbiTaxonomyID(562), // Escherichia coli
-				new NcbiTaxonomyID(11103), // Hepatitis C virus
-				new NcbiTaxonomyID(9606), // Homo sapiens
-				new NcbiTaxonomyID(10090), // Mus musculus
-				new NcbiTaxonomyID(2104), // Mycoplasma pneumoniae
-				new NcbiTaxonomyID(4530), // Oryza sativa
-				new NcbiTaxonomyID(5833), // Plasmodium falciparum
-				new NcbiTaxonomyID(4754), // Pneumocystis carinii
-				new NcbiTaxonomyID(10116), // Rattus norvegicus
-				new NcbiTaxonomyID(4932), // Saccharomyces cerevisiae
-				new NcbiTaxonomyID(4896), // Schizosaccharomyces pombe
-				new NcbiTaxonomyID(31033), // Takifugu rubripes
-				new NcbiTaxonomyID(8355), // Xenopus laevis
-				new NcbiTaxonomyID(4577) // Zea mays
-				);
-		/* @formatter:on */
-		return buildConceptMapperDictionary(workDirectory, cleanWorkDirectory, taxonomyIdsToInclude);
+
+		return buildConceptMapperDictionary(workDirectory, cleanWorkDirectory, MODEL_ORG_TAX_IDS);
+	}
+
+	public static File buildModelOrganismConceptMapperDictionary(File geneInfoFile, File outputDirectory)
+			throws IOException {
+
+		return buildConceptMapperDictionary(geneInfoFile, outputDirectory, MODEL_ORG_TAX_IDS);
+	}
+
+	public static File buildConceptMapperDictionary(File geneInfoFile, File outputDirectory,
+			Set<NcbiTaxonomyID> taxonomyIdsToInclude) throws IOException {
+		EntrezGeneInfoFileParser parser = new EntrezGeneInfoFileParser(geneInfoFile, CharacterEncoding.UTF_8);
+		return buildConceptMapperDictionary(outputDirectory, taxonomyIdsToInclude, parser);
 	}
 
 	public static File buildConceptMapperDictionary(File workDirectory, CleanDirectory cleanWorkDirectory,
 			Set<NcbiTaxonomyID> taxonomyIdsToInclude) throws IOException {
+		boolean doClean = cleanWorkDirectory.equals(CleanDirectory.YES);
+		EntrezGeneInfoFileParser parser = new EntrezGeneInfoFileParser(workDirectory, doClean);
+		return buildConceptMapperDictionary(workDirectory, taxonomyIdsToInclude, parser);
+	}
 
+	/**
+	 * @param workDirectory
+	 * @param taxonomyIdsToInclude
+	 * @param parser
+	 * @return
+	 * @throws IOException
+	 */
+	private static File buildConceptMapperDictionary(File workDirectory, Set<NcbiTaxonomyID> taxonomyIdsToInclude,
+			EntrezGeneInfoFileParser parser) throws IOException {
 		File dictionaryFile = new File(workDirectory, "cmDict-EntrezGene.xml");
 		ConceptMapperDictionaryBuilder dictBuilder = new ConceptMapperDictionaryBuilder(dictionaryFile,
 				MINIMUM_TERM_LENGTH);
 		int count = 0;
-		boolean doClean = cleanWorkDirectory.equals(CleanDirectory.YES);
-		for (EntrezGeneInfoFileParser parser = new EntrezGeneInfoFileParser(workDirectory, doClean); parser.hasNext();) {
+		while (parser.hasNext()) {
 			if (count++ % 100000 == 0)
 				logger.info("Progress: " + (count - 1));
 
