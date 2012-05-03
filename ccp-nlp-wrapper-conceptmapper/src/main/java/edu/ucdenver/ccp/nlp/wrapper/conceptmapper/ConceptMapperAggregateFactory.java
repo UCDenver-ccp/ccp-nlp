e90d8dadd836c180873ae4488fa4aece80a75a67
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-05-03 21:28:34 +0000

    Added more parameters to getOffsetTokenizerConceptMapperAggregateDescription(). Removed some methods that had the potential to allow for one tokenizer to be used by the Concept Mapper and another to be used as the first part of a tokenizer/CM aggregate AE.

--- ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/ConceptMapperAggregateFactory.java
+++ ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/ConceptMapperAggregateFactory.java
@@ -23,7 +23,6 @@ import edu.ucdenver.ccp.nlp.wrapper.conceptmapper.tokenizer.OffsetTokenizerFacto
  */
 public class ConceptMapperAggregateFactory {
 
-	
 	/**
 	 * Returns an aggregate description for a UIMA pipeline containing the OffsetTokenizer followed
 	 * by the ConceptMapper
@@ -32,63 +31,75 @@ public class ConceptMapperAggregateFactory {
 	 * @param dictionaryFile
 	 * @param caseMatchParamValue
 	 * @param searchStrategyParamValue
-	 * @param spanFeatureStructureClass 
+	 * @param spanFeatureStructureClass
+	 * @param replaceCommaWithAnd
+	 * @param findAllMatches
+	 * @param orderIndependentLookup
+	 * @param stopwordList
 	 * @return
 	 * @throws UIMAException
 	 * @throws IOException
 	 */
-	public static AnalysisEngineDescription getOffsetTokenizerConceptMapperPipelineDescription(TypeSystemDescription tsd,
-			File dictionaryFile, CaseMatchParamValue caseMatchParamValue,
-			SearchStrategyParamValue searchStrategyParamValue, Class<? extends Annotation> spanFeatureStructureClass) throws UIMAException, IOException {
+	public static AnalysisEngineDescription getOffsetTokenizerConceptMapperAggregateDescription(
+			TypeSystemDescription tsd, File dictionaryFile, CaseMatchParamValue caseMatchParamValue,
+			SearchStrategyParamValue searchStrategyParamValue, Class<? extends Annotation> spanFeatureStructureClass,
+			Class<? extends Stemmer> stemmerClass, String[] stopwordList, boolean orderIndependentLookup,
+			boolean findAllMatches, boolean replaceCommaWithAnd) throws UIMAException, IOException {
 
 		/* Init the tokenizer */
-		Object[] tokenizerConfigData = OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue);
+		Object[] tokenizerConfigData = OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue, stemmerClass);
 		AnalysisEngineDescription offsetTokenizerDescription = OffsetTokenizerFactory.buildOffsetTokenizerDescription(
 				tsd, tokenizerConfigData);
 
 		/* Init the concept mapper */
 		AnalysisEngineDescription conceptMapperDescription = ConceptMapperFactory.buildConceptMapperDescription(tsd,
-				dictionaryFile, caseMatchParamValue, searchStrategyParamValue, spanFeatureStructureClass,
+				dictionaryFile, caseMatchParamValue, searchStrategyParamValue, stemmerClass, stopwordList,
+				orderIndependentLookup, findAllMatches, replaceCommaWithAnd, spanFeatureStructureClass,
 				offsetTokenizerDescription);
 
 		return AnalysisEngineFactory.createAggregateDescription(offsetTokenizerDescription, conceptMapperDescription);
 
 	}
-	
-	
-	public static AnalysisEngineDescription buildConceptMapperOffsetTokenizerAggregate(
-			AnalysisEngineDescription conceptMapperDesc, TypeSystemDescription tsd,
-			CaseMatchParamValue caseMatchParamValue, Class<Stemmer> stemmerClass, String tokenDelimiters)
-			throws UIMAException, IOException {
 
-		Object[] tokenizerConfigData = OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue, stemmerClass,
-				tokenDelimiters);
-		AnalysisEngineDescription tokenizerDesc = OffsetTokenizerFactory.buildOffsetTokenizerDescription(tsd,
-				tokenizerConfigData);
-		return AnalysisEngineFactory.createAggregateDescription(tokenizerDesc, conceptMapperDesc);
-	}
-	
-	public static AnalysisEngineDescription buildConceptMapperOffsetTokenizerAggregate(
-			AnalysisEngineDescription conceptMapperDesc, TypeSystemDescription tsd,
-			CaseMatchParamValue caseMatchParamValue)
-			throws UIMAException, IOException {
-
-		Object[] tokenizerConfigData = OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue);
-		AnalysisEngineDescription tokenizerDesc = OffsetTokenizerFactory.buildOffsetTokenizerDescription(tsd,
-				tokenizerConfigData);
-		return AnalysisEngineFactory.createAggregateDescription(tokenizerDesc, conceptMapperDesc);
-	}
-
-	
-	public static AnalysisEngineDescription buildConceptMapperOffsetTokenizerAggregate(
-			AnalysisEngineDescription conceptMapperDesc, TypeSystemDescription tsd,
-			CaseMatchParamValue caseMatchParamValue, Class<Stemmer> stemmerClass)
-			throws UIMAException, IOException {
-
-		Object[] tokenizerConfigData = OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue, stemmerClass);
-		AnalysisEngineDescription tokenizerDesc = OffsetTokenizerFactory.buildOffsetTokenizerDescription(tsd,
-				tokenizerConfigData);
-		return AnalysisEngineFactory.createAggregateDescription(tokenizerDesc, conceptMapperDesc);
-	}
+	// public static AnalysisEngineDescription buildConceptMapperOffsetTokenizerAggregate(
+	// AnalysisEngineDescription conceptMapperDesc, TypeSystemDescription tsd,
+	// CaseMatchParamValue caseMatchParamValue, Class<Stemmer> stemmerClass, String tokenDelimiters)
+	// throws UIMAException, IOException {
+	//
+	// Object[] tokenizerConfigData =
+	// OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue, stemmerClass,
+	// tokenDelimiters);
+	// AnalysisEngineDescription tokenizerDesc =
+	// OffsetTokenizerFactory.buildOffsetTokenizerDescription(tsd,
+	// tokenizerConfigData);
+	// return AnalysisEngineFactory.createAggregateDescription(tokenizerDesc, conceptMapperDesc);
+	// }
+	//
+	// public static AnalysisEngineDescription buildConceptMapperOffsetTokenizerAggregate(
+	// AnalysisEngineDescription conceptMapperDesc, TypeSystemDescription tsd,
+	// CaseMatchParamValue caseMatchParamValue)
+	// throws UIMAException, IOException {
+	//
+	// Object[] tokenizerConfigData =
+	// OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue);
+	// AnalysisEngineDescription tokenizerDesc =
+	// OffsetTokenizerFactory.buildOffsetTokenizerDescription(tsd,
+	// tokenizerConfigData);
+	// return AnalysisEngineFactory.createAggregateDescription(tokenizerDesc, conceptMapperDesc);
+	// }
+	//
+	//
+	// public static AnalysisEngineDescription buildConceptMapperOffsetTokenizerAggregate(
+	// AnalysisEngineDescription conceptMapperDesc, TypeSystemDescription tsd,
+	// CaseMatchParamValue caseMatchParamValue, Class<Stemmer> stemmerClass)
+	// throws UIMAException, IOException {
+	//
+	// Object[] tokenizerConfigData =
+	// OffsetTokenizerFactory.buildConfigurationData(caseMatchParamValue, stemmerClass);
+	// AnalysisEngineDescription tokenizerDesc =
+	// OffsetTokenizerFactory.buildOffsetTokenizerDescription(tsd,
+	// tokenizerConfigData);
+	// return AnalysisEngineFactory.createAggregateDescription(tokenizerDesc, conceptMapperDesc);
+	// }
 
 }
