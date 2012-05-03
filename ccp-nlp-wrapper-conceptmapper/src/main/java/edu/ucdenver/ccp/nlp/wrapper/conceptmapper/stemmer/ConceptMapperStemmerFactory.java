6fc67193fda7cbc20cfd900d1992fe8b8784df3e
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-05-03 21:31:04 +0000

    Initial commit of some ConceptMapper Stemmer implementations

---
+++ ccp-nlp-wrapper-conceptmapper/src/main/java/edu/ucdenver/ccp/nlp/wrapper/conceptmapper/stemmer/ConceptMapperStemmerFactory.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package edu.ucdenver.ccp.nlp.wrapper.conceptmapper.stemmer;
+
+import org.apache.uima.conceptMapper.support.stemmer.JunkStemmer;
+import org.apache.uima.conceptMapper.support.stemmer.Stemmer;
+
+/**
+ * @author Colorado Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ *
+ */
+public class ConceptMapperStemmerFactory {
+
+	public enum StemmerType {
+		PORTER, BIOLEMMATIZER, NONE
+	}
+	
+	public static Class<? extends Stemmer> getStemmerClass(StemmerType stemmerType) {
+		switch (stemmerType) {
+		case PORTER:
+			return ConceptMapperPorterStemmer.class;
+		case BIOLEMMATIZER:
+			return ConceptMapperBioLemmatizer.class;
+		case NONE:
+			return JunkStemmer.class;
+		default:
+			throw new IllegalArgumentException("Unhandled stemmer type: " + stemmerType.name());
+		}
+	}
+	
+	
+}
