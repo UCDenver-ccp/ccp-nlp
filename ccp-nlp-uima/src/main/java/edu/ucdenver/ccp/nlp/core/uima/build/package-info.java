77b29abe515ee22ce2ec757ed01065b35e5fdb43
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-07-10 02:24:44 +0000

    Migrating under-construction code that uses annotations to automatically produce UIMA XML descriptor files as part of the build to ccp-nlp-uima-internal

--- ccp-nlp-uima/src/main/java/edu/ucdenver/ccp/nlp/core/uima/build/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The classes in this package are used during the build process to automatically generate project artifacts.<br>
- * <ul>
- * <li>{@link XmlDescriptorWriter} - automatically generates serialized versions of UIMA component descriptors 
- * in the form of XML files by looking for the "exportXmlDescriptor(File, String)" method in each Java class</li>
- * </ul>
- */
-package edu.ucdenver.ccp.nlp.core.uima.build;
-
