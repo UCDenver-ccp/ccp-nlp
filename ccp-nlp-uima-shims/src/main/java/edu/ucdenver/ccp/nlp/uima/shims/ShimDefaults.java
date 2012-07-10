969557d7acad9073bff980dd5449e5178b0b16b0
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-07-10 05:08:18 +0000

    Added a default Class constant and some comments

--- ccp-nlp-uima-shims/src/main/java/edu/ucdenver/ccp/nlp/uima/shims/ShimDefaults.java
+++ ccp-nlp-uima-shims/src/main/java/edu/ucdenver/ccp/nlp/uima/shims/ShimDefaults.java
@@ -3,12 +3,27 @@
  */
 package edu.ucdenver.ccp.nlp.uima.shims;
 
+import edu.ucdenver.ccp.nlp.uima.shims.document.impl.CcpDocumentMetadataHandler;
+import edu.ucdenver.ccp.uima.shims.document.DocumentMetadataHandler;
+
 /**
+ * This interface provides constants for various shim default values. The {@link String} constants
+ * are particularly useful when setting default values in UIMA AE configuration parameters.
+ * 
  * @author Colorado Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
  * 
  */
 public interface ShimDefaults {
 
+	/**
+	 * default class name for a {@link DocumentMetadataHandler} that complies with the CCP type
+	 * system (good for use as default values in AE configuration parameters)
+	 */
 	public static final String CCP_DOCUMENT_METADATA_HANDLER_CLASS_NAME = "edu.ucdenver.ccp.nlp.uima.shims.document.impl.CcpDocumentMetadataHandler";
+
+	/**
+	 * default class for a {@link DocumentMetadataHandler} that complies with the CCP type system
+	 */
+	public static final Class<? extends DocumentMetadataHandler> CCP_DOCUMENT_METADATA_HANDLER_CLASS = CcpDocumentMetadataHandler.class;
 
 }
