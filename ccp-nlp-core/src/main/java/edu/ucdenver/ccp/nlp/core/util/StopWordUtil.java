44bac185c5a769ab491ddae673fc5e5f83896514
Bill Baumgartner <bill.baumgartner@gmail.com>
2012-07-23 20:27:52 +0000

    added "or" to the stopword list

--- ccp-nlp-core/src/main/java/edu/ucdenver/ccp/nlp/core/util/StopWordUtil.java
+++ ccp-nlp-core/src/main/java/edu/ucdenver/ccp/nlp/core/util/StopWordUtil.java
@@ -34,15 +34,15 @@ import java.util.List;
 import edu.ucdenver.ccp.common.collections.CollectionsUtil;
 
 /**
- * @author Center for Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * @author Colorado Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
  * 
  */
 public class StopWordUtil {
 
 	public static List<String> STOPWORDS = CollectionsUtil.createList("i", "a", "is", "if", "be", "mm", "of", "no",
-			"so", "mg", "ml", "as", "we", "at", "by", "in", "on", "it", "to", "do", "kg", "km", "an", "did", "due",
-			"any", "for", "use", "may", "and", "etc", "are", "but", "can", "our", "how", "nor", "the", "has", "was",
-			"had", "its", "all", "this", "done", "just", "also", "make", "upon", "show", "used", "very", "from",
+			"so", "mg", "ml", "as", "we", "at", "by", "in", "on", "or", "it", "to", "do", "kg", "km", "an", "did",
+			"due", "any", "for", "use", "may", "and", "etc", "are", "but", "can", "our", "how", "nor", "the", "has",
+			"was", "had", "its", "all", "this", "done", "just", "also", "make", "upon", "show", "used", "very", "from",
 			"most", "were", "must", "some", "what", "have", "than", "here", "that", "such", "when", "been", "with",
 			"both", "them", "then", "into", "seem", "thus", "each", "made", "seen", "does", "they", "these", "about",
 			"quite", "again", "which", "found", "might", "shown", "using", "among", "those", "shows", "since", "while",
