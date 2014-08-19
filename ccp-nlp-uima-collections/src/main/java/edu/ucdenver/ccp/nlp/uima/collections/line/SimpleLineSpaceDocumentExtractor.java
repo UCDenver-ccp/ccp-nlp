372f71bb19ac1ace90a4ca84ece681ceb73e8216
Bill Baumgartner <bill.baumgartner@gmail.com>
2014-08-19 05:45:43 +0000

    initial commit of a line document extractor that uses a space delimiter

---
+++ ccp-nlp-uima-collections/src/main/java/edu/ucdenver/ccp/nlp/uima/collections/line/SimpleLineSpaceDocumentExtractor.java
@@ -0,0 +1,67 @@
+package edu.ucdenver.ccp.nlp.uima.collections.line;
+
+/*
+ * #%L
+ * Colorado Computational Pharmacology's common module
+ * %%
+ * Copyright (C) 2012 - 2014 Regents of the University of Colorado
+ * %%
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the Regents of the University of Colorado nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+import org.apache.log4j.Logger;
+
+import edu.ucdenver.ccp.common.string.RegExPatterns;
+import edu.ucdenver.ccp.nlp.core.document.GenericDocument;
+
+/**
+ * Designed to extract a document from a line using the following format: <br>
+ * id|text <br>
+ * 
+ * @author Colorado Computational Pharmacology, UC Denver; ccpsupport@ucdenver.edu
+ * 
+ */
+public class SimpleLineSpaceDocumentExtractor implements DocumentExtractor {
+
+	private static final Logger logger = Logger.getLogger(SimpleLineSpaceDocumentExtractor.class);
+
+	@Override
+	public GenericDocument extractDocument(String line) {
+		String[] toks = line.split(RegExPatterns.SPACE);
+		if (line.isEmpty() || toks.length < 2) {
+			logger.warn("Invalid line detected (<2 columns): " + line);
+			return null;
+		}
+		String idStr = toks[0];
+		int tabIndex = line.indexOf(" ");
+		String documentText = line.substring(tabIndex+1);
+		GenericDocument gd = new GenericDocument(idStr);
+		gd.setDocumentText(documentText);
+		return gd;
+	}
+
+}
