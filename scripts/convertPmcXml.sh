2f28be6e83eacbe54bf6a98df3ab318f0ad0ec0d
bill-baumgartner <bill.baumgartner@gmail.com>
2016-02-18 15:15:21 -0700

    added argument to run maven "offline"

--- scripts/convertPmcXml.sh
+++ scripts/convertPmcXml.sh
@@ -8,14 +8,16 @@ echo "$(basename $0) [OPTIONS]"
 echo "  [-i <nxml file or directory>]: Path to the NXML file or directory to process."
 echo "  [-s <num to skip>]: OPTIONAL. Number of files to skip prior to processing. Default=0"
 echo "  [-n <num to process>]: OPTIONAL. Number of files to process (after any that are skipped). Default = -1 (process all)."
+echo "  [-o <maven offline mode>]: OPTIONAL. If set, runs Maven in its offline mode."
 
 
 }
 
 SKIP=0
 NUM_TO_PROCESS=-1
+MAVEN_OFFLINE=""
 
-while getopts "i:n:s:" OPTION; do
+while getopts "i:n:s:o" OPTION; do
 case $OPTION in
 # The input ontology file
 i) NXML_DIR=$OPTARG
@@ -25,6 +27,9 @@ s) SKIP=$OPTARG
 ;;
 # The number of files to process (after skip)
 n) NUM_TO_PROCESS=$OPTARG
+;;
+# If set, use the -o flag when running maven
+o) MAVEN_OFFLINE=-o
 ;;
 # HELP!
 h) print_usage; exit 0
@@ -43,7 +48,7 @@ echo "Please run from the root of the project."
 exit 1
 fi
 
-mvn -e -f scripts/pom-convert-pmc-nxml.xml exec:exec \
+mvn $MAVEN_OFFLINE -e -f scripts/pom-convert-pmc-nxml.xml exec:exec \
 -DnxmlDir=$NXML_DIR \
 -Dskip=$SKIP \
 -DnumToProcess=$NUM_TO_PROCESS
